01a10fc3c56f458f8896ca57c7dd47f0fce12625
erwinschrodinger1 <81609547+erwinschrodinger1@users.noreply.github.com>
2022-09-14 01:01:49 +0545

    with added cors and things

--- index.js
+++ index.js
@@ -1,10 +1,38 @@
 const express = require("express");
 const app = express();
 const socket = require("socket.io");
-const PORT = 3000;
+const mongoose = require("mongoose");
+const cors = require("cors");
 
-app.get("/", (req, res) => {
+app.use(cors());
+
+const PORT = 5000;
+
+mongoose.connect(
+  "mongodb+srv://admin:admin@cluster0.dkdpkah.mongodb.net/?retryWrites=true&w=majority",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }
+);
+
+app.get("/api/", (req, res) => {
   res.send("Hello World!");
+});
+
+app.get("/api/list", (req, res) => {
+  res.json([
+    {
+      name: "cow1",
+      age: 1,
+      weight: 100,
+    },
+    {
+      name: "cow2",
+      age: 2,
+      weight: 200,
+    },
+  ]);
 });
 
 let server = app.listen(PORT, () => {
